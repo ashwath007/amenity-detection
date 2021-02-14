1babc49fef3636b1e3447017de7e23f9437855ff
vicky <srivicky2000@gmail.com>
2021-02-14 10:38:57 +0530

    Working on CameraApp Client - Cam options

--- App/camapp_1/App.js
+++ App/camapp_1/App.js
@@ -1,26 +1,57 @@
 import { RNCamera, FaceDetector } from 'react-native-camera';
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
   ScrollView,
   View,
   Text,
+  TouchableOpacity,
+  Button,
   StatusBar,
 } from 'react-native';
 
 
+const pendingView = () => {
+  return(
+    <View style={{flex:1 ,justifyContent:'center',alignItems:'center'}}>
+      <Text>
+        Loading ....
+      </Text>
+    </View>
+  )
+}
+
+
 
 const App = () => {
+
+  const [image, setimage] = useState(null);
+
+  const takePicture = async (camera) => {
+    try{
+      const options = {quality: 0.9, base64: false}
+      const data = await camera.takePictureAsync(options)
+      console.log(data);
+      setimage(data.uri);
+    }
+    catch(err){
+      console.warn(err);
+    }
+  }
+
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          
-        </ScrollView>
+        
+      <View>
+        <Text>
+          Cam App
+        </Text>
+      </View>
+
+
       </SafeAreaView>
     </>
   );
